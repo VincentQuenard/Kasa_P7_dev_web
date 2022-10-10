534c4f37de3dda4d2d1606ebbb45e46a8a543fb6
Quénard <vincent.quenard@free.fr>
2022-10-10 18:07:06 +0200

    pages about ok, manque event pour cacher description et rotation flèche

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import arrow from '../assets/arrow.svg';
 
-const Collapse = () => {
+const Collapse = ({title, description}) => {
   return (
     <div className='collapse_container'>
       <div className='collapse_header'>
-        <h1>collapse title</h1>
+        <h1>{title}</h1>
         <img className='collapse_arrow' src={arrow} alt='fleche_dropdown' />
       </div>
-      <p className='collapse_description'> 
-        "collapse description ou equipement"
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt at facere doloremque deleniti pariatur, voluptates quod autem sequi quis dignissimos, cumque totam impedit delectus ea minus quisquam debitis asperiores! Vel perspiciatis numquam nemo? Nam dignissimos temporibus rem ullam numquam. Veritatis explicabo cumque laboriosam delectus mollitia eaque dolorum ipsum facilis ea.
-      </p>
+      <p className='collapse_description'>{description}</p>
     </div>
   );
 };
