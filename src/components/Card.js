17de5190ade21f85a344fac41d8eebb19866eaef
Quénard <vincent.quenard@free.fr>
2022-10-11 15:01:16 +0200

    pages about ok à styliser avec effets

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,30 @@
-import React /*, { useEffect } */ from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-//import axios from 'axios';
-import locations from '../data/logements.json'
+import axios from 'axios';
+//import locations from '../data/logements.json'
 import Thumb from './Thumb';
 
-    
 const Card = () => {
-   
-    return (
-      <section className='cards_container'>
-        <div className='cards'>
-          {locations.map((location) => (
+  const [data, setData] = useState([]);
+  const getData = () => {
+    axios.get('logements.json').then((res) => setData(res.data));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <section className='cards_container'>
+      <div className='cards'>
+        {data &&
+          data.map((location) => (
             <Link key={location.id} to={`/lodging/${location.id}`}>
               <Thumb title={location.title} cover={location.cover} />
             </Link>
           ))}
-        </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Card;
--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import arrow from '../assets/arrow.svg';
 
-const Collapse = ({title, description}) => {
+const Collapse = ({ title, description }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className='collapse_container'>
       <div className='collapse_header'>
-        <h1>{title}</h1>
-        <img className='collapse_arrow' src={arrow} alt='fleche_dropdown' />
+        <h1 key={title}>{title}</h1>
+        
+        <img
+          className='collapse_arrow'
+          onClick={() => setIsOpen(!isOpen)}
+          style={{ transform: isOpen ? 'rotate(180deg)' : '' }}
+          src={arrow}
+          alt='fleche_dropdown'
+        />
       </div>
-      <p className='collapse_description'>{description}</p>
+      {isOpen && <p className='collapse_description'>{description}</p>}
     </div>
   );
 };
