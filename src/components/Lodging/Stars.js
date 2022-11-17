033a7624602f521d17c5bcbd7ab92fe1fab2ba5f
Quénard <vincent.quenard@free.fr>
2022-11-17 19:14:16 +0100

    version finale

--- src/components/Lodging/Stars.js
+++ src/components/Lodging/Stars.js
@@ -3,18 +3,19 @@ import emptyStar from '../../assets/emptyStar.svg';
 import fullStar from '../../assets/fullStar.svg';
 
 const Stars = ({ apiRating }) => {
-  //On construit un tableau vide grace au spread opérator avec 5 entrées
-  // on boucle dessus et _ retire l'index 0
-  const numberStars = [...Array(5)];
+  //On construit un tableau vide  avec 5 entrées
+  // on boucle dessus et en fonction de nombre / 5 du rating du json, étoiles pleines ou vides
+  const numberStars = [1,2,3,4,5];
   return (
     <div className='stars'>
-      {numberStars.map((_, star) =>
+      {numberStars.map((star) =>
         apiRating >= star ? (
-          <img key={star} src={fullStar} alt='full star' />
+          <img key={star.toString()} src={fullStar} alt='red-star-icon' />
         ) : (
-          <img key={star} src={emptyStar} alt='empty star' />
+          <img key={star.toString()} src={emptyStar} alt='grey-star-icon' />
         )
       )}
+     
     </div>
   );
 };
